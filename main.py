3ebbad215d223efb00b4f45fdfb523c064b2fcf6
Antonio <antoniodainok@gmail.com>
2021-01-20 10:42:21 +0100

    final commit

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 ##################### Hard Starting Project ######################
+import pandas as pd
+import datetime as dt
+import os, random, smtplib
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+# get today's day and month
+today = dt.datetime.today()
+month = today.month
+day = today.day
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# get person's day and month
+data = pd.read_csv("birthdays.csv")
+birthday_info = pd.DataFrame.to_dict(data, orient="records")
+person_month = birthday_info[0]["month"]
+person_day = birthday_info[0]["day"]
+person_name = birthday_info[0]["name"]
+
+
+# email info
+my_email = "email"
+password = "password"
+receiver = my_email
+wishes = "Happy Birthday!"
+
+
+if person_day == day and person_month == month:
+    letter = random.choice(os.listdir("letter_templates"))
+    with open(f"letter_templates/{letter}") as letter_:
+        data = letter_.readlines()
+        new_letter = [s.replace("[NAME]", person_name) for s in data]
+        letter_format = "".join(new_letter)
+    # send email
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        fmt = 'From: {}\r\nTo: {}\r\nSubject: {}\r\n{}'
+        connection.sendmail(
+            my_email,
+            receiver,
+            fmt.format(my_email, receiver, wishes, letter_format).encode('utf-8')
+        )
+
 
 
 
